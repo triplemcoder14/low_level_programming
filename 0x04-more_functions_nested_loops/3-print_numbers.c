4209c33fd3dd50adb5d872bee47456ab2c96d4cd
triplemcoder14 <mukhy16@gmail.com>
2023-02-24 03:58:01 -0800

    prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
-* print_most_numbers - prints 0 t0 9 except 2 and 4
+* print_numbers - prints numbers 0 to 9
 *
-* Return: Always 0
+* Return: void
 */
 
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == '2' || i == '4')
-			continue;
 		_putchar(i);
 	}
 
-	_putchar('\n');
+	_putchar ('\n');
 }
