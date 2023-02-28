e59421485f16605829ac645f90c94def2ccf94f9
triplemcoder14 <mukhy16@gmail.com>
2023-02-27 16:35:04 -0800

    reverse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,12 @@
-#include "main.h"
+#include <stdio.h>
+#include <string.h>
 
-/**
-* print_rev - Write a function that prints a string, in reverse,
-* followed by a new line.
-*
-* @s: character to check
-*
-* Return: (void)
-*/
-void print_rev(char *s)
-{
-int length = strlen(s);
-int i;
-for (i = length - 1 ; i >= 0; i--)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
+int _putchar(char c);
+
+void reverse_print(char *str) {
+    int len = strlen(str);
+    for (int i = len - 1; i >= 0; i--) {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
 }
