b4ef19f2ceb2f09a1c89c2fe4329a503896696eb
triplemcoder14 <mukhy16@gmail.com>
2023-03-02 15:14:46 -0800

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,26 @@
 #include "main.h"
-
 /**
- *  * _strcmp - Function that compares two strings.
- *   *
- *    * @s1: First string
- *     * @s2: Second string
- *      * Return: Value less than 0 if string is less than the other
- *       * Value greater than 0 if string is greater then the other
- *        * 0 if strings are equal
- *         */
-
+ * _strcmp - compare 2 string
+ * @s1:string
+ * @s2:strmp
+ * Return:int
+ * Bwave/ Bright
+ */
 int _strcmp(char *s1, char *s2)
 {
-		int counter, comVal;
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
 
-			counter = 0;
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
 
-				while (s1[counter] == s2[counter] && s1[counter] != '\0')
-						{
-									counter++;
-										}
-
-					comVal = s1[counter] - s2[counter];
-						return (comVal);
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
