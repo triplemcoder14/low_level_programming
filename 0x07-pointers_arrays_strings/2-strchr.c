5ac2b7c49f239d9a6fdd9862133b56e66a1bc26e
triplemcoder14 <mukhy16@gmail.com>
2023-03-06 14:22:45 -0800

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,30 +1,26 @@
+#include "main.h"
+
 /**
- * _strpbrk - a function that searches a
- *            string for any of a set of bytes.
+ * _strchr - locates a character in a string
  *
- * @s: pointer to input string
- * @accept: pointer to string we
- *         searching for in @s
+ * @c: occurrence of the character
  *
- * Return: pointer to the bytes in @s
- *         or NULL if no such byte is found
-*/
+ * @s: in the string
+ *
+ * Return: a pointer to the first occurrence of the character
+ */
 
-char *_strpbrk(char *s, char *accept)
+char *_strchr(char *s, char c)
 {
-	int i, j;
-
-	i = 0;
-	while (s[i] != '\0')
+	while (*s)
 	{
-		j = 0;
-		while (accept[j] != '\0')
-		{
-			if (s[i] == accept[j])
-				return (s + i);
-			j++;
-		}
-		i++;
+		if (*s != c)
+			s++;
+		else
+			return (s);
 	}
-	return ('\0');
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
 }
