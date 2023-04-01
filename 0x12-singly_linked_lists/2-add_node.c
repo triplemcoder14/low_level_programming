15378d68e1282a25e9b4d12b4f78d92eb52b4233
triplemcoder14 <mukhy16@gmail.com>
2023-03-31 17:04:22 -0700

    added files

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,34 +1,40 @@
 #include "lists.h"
+#include <string.h>
 
 /**
-* add_node - adds a new node at the beginning of a list_t list
-*
-* @head: pointer to pointer of list_t
-* @str: string
-*
-* Return: address of the new element, or  NULL if it faild
-*/
-
+ * add_node - Adds a new node at the beginning
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode;
-	size_t i;
+	list_t *new;
+	char *dup_str;
+	int len;
 
-	/*create memory for new node*/
-	newNode = malloc(sizeof(list_t));
-	/*return null if malloc fails*/
-	if (newNode == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	/*duplicate string to new node*/
-	newNode->str = strdup(str);
-	/*determine length of string in new node*/
-	for (i = 0; str[i]; i++)
-		;
-	/*store string length in new node*/
-	newNode->len = i;
-	/*switch addresses to add new node*/
-	newNode->next = *head;
-	*head = newNode;
 
-	return (*head);
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup_str;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
