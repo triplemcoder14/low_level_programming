1d744e9aa1cef32d6e2f26d75b01191528445ce2
triplemcoder14 <mukhy16@gmail.com>
2023-04-14 16:01:55 -0700

    indent free

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,27 +13,27 @@ int create_file(const char *filename, char *text_content)
     int fd, len, bytes_written;
 
     if (filename == NULL)
-        return (-1);
+       return (-1);
 
     fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
     if (fd == -1)
-        return (-1);
+       return (-1);
 
     if (text_content == NULL)
     {
-        close(fd);
-        return (1);
+	close(fd);
+       return (1);
     }
 
     len = 0;
     while (text_content[len] != '\0')
-        len++;
+    len++;
 
     bytes_written = write(fd, text_content, len);
     close(fd);
 
     if (bytes_written == -1 || bytes_written != len)
-        return (-1);
+       return (-1);
 
     return (1);
 }
