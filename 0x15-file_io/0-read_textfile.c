9ede3f569e353e13c93c385118382610ce7f4f69
triplemcoder14 <mukhy16@gmail.com>
2023-04-14 15:07:44 -0700

    updated code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,51 @@
 #include "main.h"
 
 /**
-* read_textfile - a function that reads a text file
-*and prints it to the POSIX standard output
-* @filename: the file name
-* @letters: the number of letters
-* Return: the actual number of letters or 0 if fails
-**/
-
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: the file name
+ * @letters: the number of letters
+ * Return: the number of characters read and printed, or 0 if an error occurred
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f, length, i, res;
-	char *buffer;
+    int f;
+    ssize_t read_bytes, write_bytes;
+    size_t i;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
-	/* open */
+    if (filename == NULL)
+        return (0);
 
-	f = open(filename, O_RDONLY);
+    f = open(filename, O_RDONLY);
+    if (f == -1)
+        return (0);
 
-	if (f == -1)
-		return (0);
+    buffer = malloc(sizeof(char) * letters);
+    if (!buffer) {
+        close(f);
+        return (0);
+    }
 
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
-		return (0);
+    read_bytes = read(f, buffer, letters);
+    if (read_bytes == -1) {
+        close(f);
+        free(buffer);
+        return (0);
+    }
 
-	read(f, buffer, letters);
-	buffer[letters] = '\0';
+    close(f);
 
-	for (i = 0; buffer[i] != '\0'; i += 1)
-		length += 1;
+    for (i = 0; i < (size_t)read_bytes; i++)
+    {
+        write_bytes = write(STDOUT_FILENO, &buffer[i], 1);
+        if (write_bytes != 1) {
+            free(buffer);
+            return (0);
+        }
+    }
 
-	res = close(f);
-	if (res != 0)
-		exit(-1);
-	res = write(STDOUT_FILENO, buffer, length);
-	if (res != length)
-		return (0);
-	free(buffer);
+    free(buffer);
 
-	return (length);
+    return (read_bytes);
 }
+
