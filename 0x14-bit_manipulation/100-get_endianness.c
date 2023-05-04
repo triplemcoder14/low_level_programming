ec12e08e4ac35e8adeee123cf76fc52a1b0b7dac
mukhy <mukhy16@gmail.com>
2023-05-04 15:59:05 -0700

    indent free

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,14 +7,18 @@
  * Return: 0 if big endian, 1 if little endian
  */
 
-int get_endianness(void) {
-    unsigned int x = 0x12345678;
-    char* ptr = (char*)&x;
+int get_endianness(void)
+{
+	unsigned int x = 0x12345678;
+	char *ptr = (char *)&x;
 
-  
-    if (*ptr == 0x78) {
-        return 1; /** little endian */
-    } else {
-        return 0; /** big endian */
-    }
+
+	if (*ptr == 0x78)
+	{
+		return (1); /** little endian */
+	}
+	else
+	{
+		return (0); /** big endian */
+	}
 }
